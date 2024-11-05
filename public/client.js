90031711aa3d12898bfc3d9ff71ff1e2034d417c
stagas <gstagas@gmail.com>
2024-11-05 23:24:40 +0200

    build

--- public/client.js
+++ public/client.js
@@ -2,12 +2,12 @@
 <div class="logo">xpress.wtf</div>
 
 <div class="nav">
-  <a href="#">txts</a>
-  <a href="#">phots</a>
-  <a href="#">vids</a>
-  <a href="#">snds</a>
-  <a href="#">wtfs</a>
+  <a href="#">hang</a>
+  <a href="#">show</a>
+  <a href="#">tell</a>
+  <a href="#">rant</a>
+  <a href="#">wtf</a>
 </div>
 
 `;})();
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiY2xpZW50LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJkb2N1bWVudC5ib2R5LmlubmVySFRNTCA9IC8qaHRtbCovYFxuPGRpdiBjbGFzcz1cImxvZ29cIj54cHJlc3Mud3RmPC9kaXY+XG5cbjxkaXYgY2xhc3M9XCJuYXZcIj5cbiAgPGEgaHJlZj1cIiNcIj50eHRzPC9hPlxuICA8YSBocmVmPVwiI1wiPnBob3RzPC9hPlxuICA8YSBocmVmPVwiI1wiPnZpZHM8L2E+XG4gIDxhIGhyZWY9XCIjXCI+c25kczwvYT5cbiAgPGEgaHJlZj1cIiNcIj53dGZzPC9hPlxuPC9kaXY+XG5cbmBcbiJdLAogICJtYXBwaW5ncyI6ICJtQkFBQSxTQUFTLEtBQUssVUFBb0I7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTsiLAogICJuYW1lcyI6IFtdCn0K
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiY2xpZW50LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJkb2N1bWVudC5ib2R5LmlubmVySFRNTCA9IC8qaHRtbCovYFxuPGRpdiBjbGFzcz1cImxvZ29cIj54cHJlc3Mud3RmPC9kaXY+XG5cbjxkaXYgY2xhc3M9XCJuYXZcIj5cbiAgPGEgaHJlZj1cIiNcIj5oYW5nPC9hPlxuICA8YSBocmVmPVwiI1wiPnNob3c8L2E+XG4gIDxhIGhyZWY9XCIjXCI+dGVsbDwvYT5cbiAgPGEgaHJlZj1cIiNcIj5yYW50PC9hPlxuICA8YSBocmVmPVwiI1wiPnd0ZjwvYT5cbjwvZGl2PlxuXG5gXG4iXSwKICAibWFwcGluZ3MiOiAibUJBQUEsU0FBUyxLQUFLLFVBQW9CO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7IiwKICAibmFtZXMiOiBbXQp9Cg==
