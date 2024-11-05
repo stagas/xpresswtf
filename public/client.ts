06bddc6bdc02143ced5274e6dbbcebea491fc180
stagas <gstagas@gmail.com>
2024-11-05 23:34:10 +0200

    feat: readd tell

--- public/client.ts
+++ public/client.ts
@@ -4,6 +4,7 @@ document.body.innerHTML = /*html*/`
 <div class="nav">
   <a href="#" style="color:#8f8">hang</a>
   <a href="#" style="color:#f7b">show</a>
+  <a href="#" style="color:#0ff">tell</a>
   <a href="#" style="color:#ff0">rant</a>
   <a href="#" style="color:#f62">wtf</a>
 </div>
