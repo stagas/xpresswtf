dcbff30f9f93403f6f55ccec725a310b7c43c2ad
stagas <gstagas@gmail.com>
2024-11-05 23:14:50 +0200

    build

--- public/client.js
+++ public/client.js
@@ -1,2 +1,12 @@
-"use strict";(()=>{document.body.innerHTML="hello fuckin world";})();
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiY2xpZW50LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJkb2N1bWVudC5ib2R5LmlubmVySFRNTCA9IGBoZWxsbyBmdWNraW4gd29ybGRgXG4iXSwKICAibWFwcGluZ3MiOiAibUJBQUEsU0FBUyxLQUFLLFVBQVkiLAogICJuYW1lcyI6IFtdCn0K
+"use strict";(()=>{document.body.innerHTML=`
+<div class="logo">xpress.wtf</div>
+
+<div class="nav">
+  <a href="#">txts</a>
+  <a href="#">phots</a>
+  <a href="#">vids</a>
+  <a href="#">snds</a>
+</div>
+
+`;})();
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiY2xpZW50LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJkb2N1bWVudC5ib2R5LmlubmVySFRNTCA9IC8qaHRtbCovYFxuPGRpdiBjbGFzcz1cImxvZ29cIj54cHJlc3Mud3RmPC9kaXY+XG5cbjxkaXYgY2xhc3M9XCJuYXZcIj5cbiAgPGEgaHJlZj1cIiNcIj50eHRzPC9hPlxuICA8YSBocmVmPVwiI1wiPnBob3RzPC9hPlxuICA8YSBocmVmPVwiI1wiPnZpZHM8L2E+XG4gIDxhIGhyZWY9XCIjXCI+c25kczwvYT5cbjwvZGl2PlxuXG5gXG4iXSwKICAibWFwcGluZ3MiOiAibUJBQUEsU0FBUyxLQUFLLFVBQW9CO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBOyIsCiAgIm5hbWVzIjogW10KfQo=
