9563596452f1a83009a6fdc68944615fd6dc102e
stagas <gstagas@gmail.com>
2024-11-05 23:33:12 +0200

    feat: remove tell

--- public/client.ts
+++ public/client.ts
@@ -2,11 +2,10 @@ document.body.innerHTML = /*html*/`
 <div class="logo">xpress.wtf</div>
 
 <div class="nav">
-  <a href="#">hang</a>
-  <a href="#">show</a>
-  <a href="#">tell</a>
-  <a href="#">rant</a>
-  <a href="#">wtf</a>
+  <a href="#" style="color:#8f8">hang</a>
+  <a href="#" style="color:#f7b">show</a>
+  <a href="#" style="color:#ff0">rant</a>
+  <a href="#" style="color:#f62">wtf</a>
 </div>
 
 `
