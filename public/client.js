cb3113ef4acb4adca444c20c18071976f6cb6d58
stagas <gstagas@gmail.com>
2024-11-06 11:11:31 +0200

    build

--- public/client.js
+++ public/client.js
@@ -2,12 +2,12 @@
 <div class="logo">xpress.wtf</div>
 
 <div class="nav">
-  <a href="#" style="color:#8f8">hang</a>
-  <a href="#" style="color:#f7b">show</a>
-  <a href="#" style="color:#0ff">tell</a>
-  <a href="#" style="color:#ff0">rant</a>
-  <a href="#" style="color:#f62">wtf</a>
+  <a href="#" style="color:#8d5">hang<span class="nots">395</span></a>
+  <a href="#" class="active" style="color:#f8f">show<span class="nots">25</span></a>
+  <a href="#" style="color:#0ff">tell<span class="nots zero">&nbsp;</span></a>
+  <a href="#" style="color:#ec5">rant<span class="nots">5</span></a>
+  <a href="#" style="color:#f50">wtf<span class="nots">15</span></a>
 </div>
 
 `;})();
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiY2xpZW50LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJkb2N1bWVudC5ib2R5LmlubmVySFRNTCA9IC8qaHRtbCovYFxuPGRpdiBjbGFzcz1cImxvZ29cIj54cHJlc3Mud3RmPC9kaXY+XG5cbjxkaXYgY2xhc3M9XCJuYXZcIj5cbiAgPGEgaHJlZj1cIiNcIiBzdHlsZT1cImNvbG9yOiM4ZjhcIj5oYW5nPC9hPlxuICA8YSBocmVmPVwiI1wiIHN0eWxlPVwiY29sb3I6I2Y3YlwiPnNob3c8L2E+XG4gIDxhIGhyZWY9XCIjXCIgc3R5bGU9XCJjb2xvcjojMGZmXCI+dGVsbDwvYT5cbiAgPGEgaHJlZj1cIiNcIiBzdHlsZT1cImNvbG9yOiNmZjBcIj5yYW50PC9hPlxuICA8YSBocmVmPVwiI1wiIHN0eWxlPVwiY29sb3I6I2Y2MlwiPnd0ZjwvYT5cbjwvZGl2PlxuXG5gXG4iXSwKICAibWFwcGluZ3MiOiAibUJBQUEsU0FBUyxLQUFLLFVBQW9CO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7IiwKICAibmFtZXMiOiBbXQp9Cg==
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiY2xpZW50LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJkb2N1bWVudC5ib2R5LmlubmVySFRNTCA9IC8qaHRtbCovYFxuPGRpdiBjbGFzcz1cImxvZ29cIj54cHJlc3Mud3RmPC9kaXY+XG5cbjxkaXYgY2xhc3M9XCJuYXZcIj5cbiAgPGEgaHJlZj1cIiNcIiBzdHlsZT1cImNvbG9yOiM4ZDVcIj5oYW5nPHNwYW4gY2xhc3M9XCJub3RzXCI+Mzk1PC9zcGFuPjwvYT5cbiAgPGEgaHJlZj1cIiNcIiBjbGFzcz1cImFjdGl2ZVwiIHN0eWxlPVwiY29sb3I6I2Y4ZlwiPnNob3c8c3BhbiBjbGFzcz1cIm5vdHNcIj4yNTwvc3Bhbj48L2E+XG4gIDxhIGhyZWY9XCIjXCIgc3R5bGU9XCJjb2xvcjojMGZmXCI+dGVsbDxzcGFuIGNsYXNzPVwibm90cyB6ZXJvXCI+Jm5ic3A7PC9zcGFuPjwvYT5cbiAgPGEgaHJlZj1cIiNcIiBzdHlsZT1cImNvbG9yOiNlYzVcIj5yYW50PHNwYW4gY2xhc3M9XCJub3RzXCI+NTwvc3Bhbj48L2E+XG4gIDxhIGhyZWY9XCIjXCIgc3R5bGU9XCJjb2xvcjojZjUwXCI+d3RmPHNwYW4gY2xhc3M9XCJub3RzXCI+MTU8L3NwYW4+PC9hPlxuPC9kaXY+XG5cbmBcbiJdLAogICJtYXBwaW5ncyI6ICJtQkFBQSxTQUFTLEtBQUssVUFBb0I7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTsiLAogICJuYW1lcyI6IFtdCn0K
