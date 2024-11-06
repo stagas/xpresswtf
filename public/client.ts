f23eb17261b98ce88d4345d0fa1d677ecb3725e5
stagas <gstagas@gmail.com>
2024-11-06 11:10:41 +0200

    notifs

--- public/client.ts
+++ public/client.ts
@@ -2,11 +2,11 @@ document.body.innerHTML = /*html*/`
 <div class="logo">xpress.wtf</div>
 
 <div class="nav">
-  <a href="#" style="color:#8f8">hang</a>
-  <a href="#" style="color:#f7b">show</a>
-  <a href="#" style="color:#0ff">tell</a>
-  <a href="#" style="color:#ff0">rant</a>
-  <a href="#" style="color:#f62">wtf</a>
+  <a href="#" style="color:#8d5">hang<span class="nots">395</span></a>
+  <a href="#" class="active" style="color:#f8f">show<span class="nots">25</span></a>
+  <a href="#" style="color:#0ff">tell<span class="nots zero">&nbsp;</span></a>
+  <a href="#" style="color:#ec5">rant<span class="nots">5</span></a>
+  <a href="#" style="color:#f50">wtf<span class="nots">15</span></a>
 </div>
 
 `
